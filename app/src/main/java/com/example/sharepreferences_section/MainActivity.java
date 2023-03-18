10dc7654b7cbbdc79b3b04ea77793c6e4c2c2156
kamel <kamelhassan462@gmail.com>
2023-03-18 17:32:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sharepreferences_section/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.sharepreferences_section;
+
+import android.app.AppComponentFactory;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button   = findViewById(R.id.button);
+
+        DisplaySavedText();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredText = editText.getText().toString();
+                DisplayAndSaveText(enteredText);
+            }
+        });
+
+
+
+
+    }
+
+    private void DisplaySavedText() {
+        // Retrieving the value from SharedPref
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("MySharedPref",MODE_PRIVATE);
+
+        String s1 = sharedPreferences.getString("name"," ");
+
+        textView.setText(s1);
+
+
+    }
+
+    private void DisplayAndSaveText(String enteredText) {
+
+        // Display the Text
+        textView.setText(enteredText);
+
+        // Saving the Text into SharedPref
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("MySharedPref",MODE_PRIVATE);
+
+        // Writing data to shared pref
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("name",enteredText);
+
+        editor.commit();
+    }
+}
